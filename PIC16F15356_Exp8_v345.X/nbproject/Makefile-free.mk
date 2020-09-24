213ca13627c8a6920cfd93815ab389326482f09e
Nalini Yoganandan - I00190 <nalini.yoganandan@microchip.com>
2020-09-24 13:13:56 +0530

    change device name

--- PIC16F15356_Exp8_v345.X/nbproject/Makefile-free.mk
+++ PIC16F15356_Exp8_v345.X/nbproject/Makefile-free.mk
@@ -90,7 +90,7 @@ ifneq ($(INFORMATION_MESSAGE), )
 endif
 	${MAKE}  -f nbproject/Makefile-free.mk dist/${CND_CONF}/${IMAGE_TYPE}/PIC16F15356_Exp8_v345.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
-MP_PROCESSOR_OPTION=16F15356
+MP_PROCESSOR_OPTION=16LF15356
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
